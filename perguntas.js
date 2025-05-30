d9519917a6f1dfbeac983992e2394a096df1f589
milly1516 <149428556+milly1516@users.noreply.github.com>
2025-05-30 09:34:48 -0300

    b

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Inglês ',
+    'Qual a diferença entre "much" e "many" em inglês?',
+    'Much" é usado com substantivos incontáveis (ex: "much water", "much sugar") e "many" é usado com substantivos contáveis (ex: "many apples", "many books").'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    '"girias do iglês',
+    'O que significa a expressão "break the ice"?',
+    'Break the ice" significa iniciar uma conversa ou atividade para aliviar a tensão ou desconforto em uma situação social, geralmente em um encontro ou reunião.'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Present perfect',
+    ' Quando usamos o "present perfect" em inglês?',
+    ' O "present perfect" é usado para falar sobre ações que ocorreram no passado, mas têm relevância no presente. Exemplo: "I have eaten breakfast already."'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    '"to" e "for"',
+    'Qual é a diferença entre "to" e "for"?',
+    '"To" é usado para indicar direção, movimento ou finalidade (ex: "I am going to the store"), enquanto "for" é usado para indicar propósito, benefício ou duração (ex: "This gift is for you", "I have lived here for five years").'
 )
