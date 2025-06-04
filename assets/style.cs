62a28e9a9ec8e86203b905b4db4d26780681ebdc
milly1516 <149428556+milly1516@users.noreply.github.com>
2025-06-04 09:24:42 -0300

    mn

--- assets/style.cs
+++ assets/style.cs
@@ -99,7 +99,7 @@ footer p {
 @media (max-width: 560px) {
     
     body {
-        background: url('img/bg-mobile.webp'), no-repeat cover;
+        background: url('img/ingles.jpg'), no-repeat cover;
     }
 
     .cartao {
